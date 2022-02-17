333048e126950d220a564343b7c905515f29d56a
Behrouz Talavari <b.talavari@persici.co.uk>
2022-02-17 22:41:35 +0330

    Add State Pattern

---
+++ Patterns/MementoPattern/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace MementoPattern
+{
+    public class CarState
+    {
+        public int Speed { get; set; }
+        public int Fule { get; set; }
+
+    }
+    public class Car
+    {
+        private int _fule;
+        private int _speed;
+
+        public CarState CarState { get; }
+
+        public void AddFule(int value)
+        {
+            _fule += value;
+        }
+        public void IncreaseSpeed()
+        {
+            _speed++;
+        }
+        public void DecreaseSpeed()
+        {
+            _speed--;
+        }
+        public void Kilometer()
+        {
+            _fule -= 6;
+        }
+        public Car()
+        {
+
+        }
+        public Car(CarState carState)
+        {
+            CarState = carState;
+        }
+        public CarState GetCarState()
+        {
+            return new CarState
+            {
+                Speed = _speed,
+                Fule = _fule
+            };
+
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Patterns/ObserverPattern/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace StatePattern
+{
+    public interface IState
+    {
+        bool ChangeState();
+
+    }
+    public class OnState : IState
+    {
+        public bool ChangeState()
+        {
+            Console.WriteLine("Turned it Off");
+            return false;
+        }
+    }
+    public class OffState : IState
+    {
+        public bool ChangeState()
+        {
+            Console.WriteLine("Turned it On");
+            return true;
+        }
+    }
+
+    public class TV
+    {
+        private bool isOn;
+        private IState state;
+        public bool IsOn { 
+            get 
+            {
+                return isOn; 
+            } 
+            set 
+            { 
+                isOn = value;
+                state = isOn ? new OnState() : new OffState();
+            } 
+        }
+        public TV()
+        {
+            state = new OffState();
+        }
+        public void OnOffBtnPush()
+        {
+            IsOn = state.ChangeState();
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tv = new TV();
+            tv.OnOffBtnPush();
+            tv.OnOffBtnPush();
+            tv.OnOffBtnPush();
+            tv.OnOffBtnPush();
+            Console.ReadKey();
+        }
+    }
+}
